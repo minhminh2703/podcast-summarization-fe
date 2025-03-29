4eff2f256ed8bd7c531f689f1070708191d47e3f
Minh Minh <nguyenthiminhminh6a1@gmail.com>
2025-03-30 00:56:10 +0700

    feat: finish custom UI of tab

--- src/features/podcast-summarize/index.tsx
+++ src/features/podcast-summarize/index.tsx
@@ -1,10 +1,11 @@
-import { Link } from 'react-router-dom';
 import { Box, Tab, Tabs } from '@mui/material';
 import { useState } from 'react';
 import CustomTabPanel from '../../components/custom-tab-panel';
 import SummarizeLinkInput from './components/summarize-link';
 import SummarizeFileInput from './components/summarize-file';
-// import SummarizeFileInput from './components/summarize-file';
+import LinkIcon from '@mui/icons-material/Link';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { BorderBottom } from '@mui/icons-material';
 
 enum TabValue {
   SUMMARIZE_LINK = 0,
@@ -19,23 +20,48 @@ const PodcastSummarize: React.FC = () => {
   };
 
   return (
-    <Box>
+    <Box sx={{ width: '100%' }}>
       <Tabs
         value={selectedTab}
         onChange={handleTabChange}
-        indicatorColor="primary"
-        textColor="primary"
-        variant="fullWidth"
-        sx={{ marginBottom: 7, backgroundColor: 'white' }}
+        textColor="inherit"
+        variant="standard"
+        sx={{ maxHeight: 50, padding: 0, marginLeft: 4, paddingLeft: 10, marginBottom: 5 }}
+        slotProps={{ indicator: { style: { backgroundColor: '#5B913B', height: 2 } } }}
+
       >
-        <Tab label="Summarize Link" value={TabValue.SUMMARIZE_LINK} />
-        <Tab label="Summarize File" value={TabValue.SUMMARIZE_FILE} />
+        <Tab icon={<LinkIcon />} iconPosition="start" label='Link' value={TabValue.SUMMARIZE_LINK} sx={{
+          color: 'white',
+          paddingBottom: 2,
+          fontFamily: 'IBM Plex Mono',
+          fontSize: '1em',
+          fontWeight: '600',
+          ":focus": {
+            outline: 'none',
+          },
+          "&.Mui-selected": {
+            BorderBottom: 'none',
+          }
+        }} />
+        <Tab icon={<AttachFileIcon />} iconPosition="start" label='File upload' value={TabValue.SUMMARIZE_FILE} sx={{
+          color: 'white',
+          paddingBottom: 2,
+          fontFamily: 'IBM Plex Mono',
+          fontSize: '1em',
+          fontWeight: '600',
+          ":focus": {
+            outline: 'none',
+          },
+          "&.Mui-selected": {
+            BorderBottom: 'none',
+          }
+        }} />
       </Tabs>
 
       <CustomTabPanel value={selectedTab} index={TabValue.SUMMARIZE_LINK}>
         <SummarizeLinkInput />
       </CustomTabPanel>
-      
+
       <CustomTabPanel value={selectedTab} index={TabValue.SUMMARIZE_FILE}>
         <SummarizeFileInput />
       </CustomTabPanel>
